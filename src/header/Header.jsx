f761a3832afb66347e2af257679bec5561c9c74a
Dartz464 <niltondz464@outlook.com>
2023-01-07 02:54:29 -0300

    Atualizando Header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,32 +1,38 @@
 import './header.css';
 import React, {useState} from 'react';
+import menuImg from './menu.png';
 
 function Header() {
 
-    // Controlar navbar
-    const [navbar, setNavbar] = useState(false);
+    // Controlar menu
+    const [headerActive, setHeader] = useState(false);
     const prevScrollpos = window.pageYOffset;
-
-    const showNavbar =() =>{
+    const showHeader =() =>{
         const currentScrollPos = window.pageYOffset;
         if(currentScrollPos > prevScrollpos){
-            setNavbar(true);
+            setHeader(true);
         }else{
-            setNavbar(false);
+            setHeader(false);
         }
     };
+    window.addEventListener('scroll', showHeader);
 
-    window.addEventListener('scroll', showNavbar);
+    // Controlar menu mobile
+    const [mobileActive, setMobile] = useState(false);
+    const ShowMobile = ()=>{
+        mobileActive ? setMobile(false) : setMobile(true);
+    }
 
     return (
-        <header>
-            <nav className={navbar ? 'hide' : ''}>
-                <ul>
-                    <li className='li-hover'>Pomodoro</li>
-                    <li className='li-hover'>O que é?</li>
-                    <li className='li-hover'>Sobre</li>
-                    <li>|</li>
-                    <li className='li-hover'>Configurações</li>
+        <header id='header' className={headerActive ? 'header-active' : ''}>
+            <h1 id='logo'>POMORIN</h1>
+            <nav id='nav-bar' className={mobileActive ? 'mobile-active' : ''}>
+                <img src={menuImg} alt="" id='btn-mobile' onClick={ShowMobile}/>
+                {/* <button >Menu</button> */}
+                <ul id='menu'>
+                    <li><a href="">Pomodoro</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Novidades</a></li>
                 </ul>
             </nav>
         </header>
